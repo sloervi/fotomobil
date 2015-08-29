8ecd6ebdf159325c57e4592132591d6d1da07053
sloervi McMurphy <sloervi@users.noreply.github.com>
2015-08-29 16:48:48 +0200

    Create fotomobil.sh

---
+++ fotomobil.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# fotomobil.sh
+# sloervi McMurphy
+# Make a copy of your photos with a width of 1200 pixel
+
+for ii in `find . -type d `
+do
+        cd $ii
+        pwd
+        /usr/local/bin/fotomobil/fotomobil.pl
+done
